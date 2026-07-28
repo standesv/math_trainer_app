10f8dc117fdc58f72c6bfcb750418aa2c8fa5a14
Stanislas de Saint-Vaulry <sdargier@adequasys.com>
2026-07-28 11:31:38 +0200

    Ajoute l import manquant de Modifier.background

--- app/src/main/java/com/standesv/mathtrainer/ui/Screens.kt
+++ app/src/main/java/com/standesv/mathtrainer/ui/Screens.kt
@@ -1,5 +1,6 @@
 package com.standesv.mathtrainer.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
